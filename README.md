e1e65fa192673d3507787bef1edc22fd7c58e9e4
David Durman <daviddurman@gmail.com>
2024-02-28 14:30:12 +0100

    Update README; Remove domain filter

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Appmixer Embedded Integrations Demo
+# Appmixer Embedded Designer Demo
 
 Try live at:
 
 ```
-https://demo-integrations.appmixer.com?apiUrl=https://api.YOUR_TENANT.appmixer.com&studioUrl=https://my.YOUR_TENANT.appmixer.com
+https://demo-designer.appmixer.com?apiUrl=https://api.YOUR_TENANT.appmixer.com&studioUrl=https://my.YOUR_TENANT.appmixer.com
 ```
 
 The following query parameters are supported:
@@ -14,7 +14,7 @@ The following query parameters are supported:
 | `studioUrl`  | ✅ | The Appmixer Tenant Studio URL that you were assigned at sign-up.  | `https://my.YOUR_TENANT.appmixer.com` |
 | `username`  |  | An Appmixer Virtual User Username. If not defined, a virtual user with a the following username will be created `embed-demo-1234@appmixer-embed-integrations.com` (for demo purposes).  | `my-virtual-user123@myapp.com` |
 | `token`  |  | An Appmixer Virtual User Token.  | `d17e3798-d754-4397-aff2-f06e71c5ae57` |
-| `domainFilter`  |  | If specified, only integrations pubslihed to this specific domain will be displayed. Otherwise, integrations published to all users will be displayed.  | `myapp.com` |
 
 
-![Screenshot](assets/screenshot.png?raw=true "Screenshot")
+![Screenshot](assets/screenshot-flow-manager.png?raw=true "Screenshot")
+![Screenshot](assets/screenshot-designer.png?raw=true "Screenshot")
--- main.js
+++ main.js
@@ -8,7 +8,6 @@ if (!APPMIXER_API_URL) {
 
 let APPMIXER_VIRTUAL_USER_USERNAME = QUERY_PARAMS.get('username');
 let APPMIXER_VIRTUAL_USER_TOKEN;
-let DOMAIN_FILTER = QUERY_PARAMS.get('domainFilter');
 
 if (APPMIXER_VIRTUAL_USER_USERNAME) {
     // If username is provided in the query string of this demo, password must be provided as well.
@@ -18,9 +17,9 @@ if (APPMIXER_VIRTUAL_USER_USERNAME) {
     // Note that Appmixer username can be any, even non-existing, email address. We're using a user ID
     // together with a fictional domain name. Appmixer does not send anything to these email addresses.
     // They are just used as a virtual user credentials pair. Moreover, the email domain
-    // allows us to easily share integration templates with a specific group of users
+    // allows us to easily share automations with a specific group of users
     // (An alternative to this is to use the user scopes.)
-    APPMIXER_VIRTUAL_USER_USERNAME = 'embed-demo-1234@appmixer-embed-integrations.com';
+    APPMIXER_VIRTUAL_USER_USERNAME = 'embed-demo-1234@appmixer-embed-designer.com';
     APPMIXER_VIRTUAL_USER_TOKEN = '4efaa8ec-ddc5-4852-b6cc-cb3039fe17b1';
 }
 
@@ -30,15 +29,12 @@ if (typeof Appmixer === 'undefined') {
 
 // Appmixer SDK instance.
 const appmixer = new Appmixer({
-    baseUrl: APPMIXER_API_URL,
-    theme: {
-        variables: { colors: { surface: '#f5f5f5' } }
-    }
+    baseUrl: APPMIXER_API_URL
 });
 
 const widgets = {
-    integrations: null,
-    wizard: null
+    automations: null,
+    designer: null
 };
 
 // Learn more about Appmixer virtual users at https://docs.appmixer.com/appmixer/tutorials/appmixer-virtual-users.
@@ -63,43 +59,56 @@ async function ensureAppmixerVirtualUser(username, token) {
 }
 
 function createWidgets() {
-    // Create Integrations Page widget.
-    widgets.integrations = appmixer.ui.Integrations({
-        el: '#appmixer-integrations-marketplace',
+    widgets.automations = appmixer.ui.FlowManager({
+        el: '#automations-placeholder',
         options: {
-            showHeader: true,
-            customFilter: [{
-                // Show only integration templates shared with users in this demo app.
-                ...(DOMAIN_FILTER && { 'sharedWith.0.domain': DOMAIN_FILTER }),
-                type: 'integration-template'
-            }, {
-                userId: appmixer.get('user').id,
-                type: 'integration-instance'
-            }]
+            customFilter: {
+                'customFields.tag': 'appmixer-demo-embedded-designer'
+            },
+            menu: [
+                { event: 'flow:remove', label: 'Remove' }
+            ]
         }
     });
-    widgets.integrations.on('integration:create', templateId => {
-        widgets.wizard.close();
-        widgets.wizard.set('flowId', templateId);
-        widgets.wizard.open();
+    widgets.designer = appmixer.ui.Designer({
+        el: '#designer-placeholder',
+        options: {
+            showButtonHome: true,
+            menu: [
+                { event: 'flow:rename', label: 'Rename' }
+            ],
+            toolbar: [
+                ['undo', 'redo'],
+                ['zoom-to-fit', 'zoom-in', 'zoom-out'],
+                ['logs']
+            ]
+        }
     });
-    widgets.integrations.on('integration:edit', integrationId => {
-        widgets.wizard.close();
-        widgets.wizard.set('flowId', integrationId);
-        widgets.wizard.open();
+
+    widgets.automations.on('flow:create', async () => {
+        const flowId = await appmixer.api.createFlow('New Flow', {}, { customFields: { tag: 'appmixer-demo-embedded-designer' } });
+        widgets.designer.close();
+        widgets.designer.set('flowId', flowId);
+        widgets.automations.close();
+        widgets.designer.open();
     });
-    widgets.wizard = appmixer.ui.Wizard();
-    widgets.wizard.on('flow:start-after', () => widgets.integrations.reload());
-    widgets.wizard.on('flow:remove-after', () => {
-        widgets.integrations.reload();
-        widgets.wizard.close();
+
+    widgets.automations.on('flow:open', flowId => {
+        widgets.designer.close();
+        widgets.designer.set('flowId', flowId);
+        widgets.designer.open();
+        widgets.automations.close();
+    });
+    widgets.designer.on('navigate:flows', () => {
+        widgets.designer.close();
+        widgets.automations.open();
     });
 }
 
 async function main() {
     await ensureAppmixerVirtualUser(APPMIXER_VIRTUAL_USER_USERNAME, APPMIXER_VIRTUAL_USER_TOKEN);
     createWidgets();
-    widgets.integrations.open();
+    widgets.automations.open();
 
     document.querySelector('.app-event-form').addEventListener('submit', (evt) => {
         evt.preventDefault();
